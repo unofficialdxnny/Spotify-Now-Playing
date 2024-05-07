e4ad16de2e01b916b6223f22390b920d2b778391
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-05-07 19:27:48 +0100

    nicer UI

--- SNP/script.js
+++ SNP/script.js
@@ -1,14 +1,24 @@
 const clientId = '1ca36fe2f4b345a8b7b54238868e2318';
-const redirectUri = 'http://localhost:5500/SNP/';
+const redirectUri = 'http://localhost:5500/SNP/callback.html';
 const clientSecret = '7bf337a0dc774655a7bce821c7a847d1'
-
 
 const trackInfoText = document.getElementById('track-info');
 
-// Function to authorize Spotify
-function authorizeSpotify() {
-    // Redirect user to Spotify authorization page
-    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-currently-playing`;
+// Function to update track information
+function updateTrackInfo(trackData) {
+    const albumCover = document.getElementById('album-cover');
+    const songName = document.getElementById('song-name');
+    const artistName = document.getElementById('artist-name');
+    
+    albumCover.src = trackData.album.images[0].url;
+    songName.textContent = trackData.name;
+    artistName.textContent = trackData.artists[0].name;
+}
+
+// Function to update progress bar
+function updateProgressBar(progress) {
+    const progressBar = document.getElementById('progress-bar');
+    progressBar.style.width = `${progress * 100}%`;
 }
 
 // Function to fetch currently playing track
@@ -26,15 +36,19 @@ function fetchCurrentlyPlayingTrack(accessToken) {
         }
     })
     .then(data => {
-        const trackName = data.item.name;
-        const artistName = data.item.artists[0].name;
-        const trackInfo = `${trackName} by ${artistName}`;
-        trackInfoText.textContent = trackInfo;
+        updateTrackInfo(data.item);
+        updateProgressBar(data.progress_ms / data.item.duration_ms);
     })
     .catch(error => {
         console.error('Error fetching currently playing track:', error);
         trackInfoText.textContent = "Error fetching currently playing track";
     });
+}
+
+// Function to authorize Spotify
+function authorizeSpotify() {
+    // Redirect user to Spotify authorization page
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=user-read-currently-playing`;
 }
 
 // Check if there's an access token in the URL (user just authenticated)
